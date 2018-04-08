8cc6b7cc6cac21d68aaedead05af60e115bd4cdb
mamamoo0619 <sksbthwjd@hanmail.net>
2018-04-09 01:40:19 +0900

    Cookiecutter + cleanup

---
+++ nomadgram/images/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from nomadgram.users import models as user_models
+
+class TimeStampedModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Image(TimeStampedModel):
+
+    """ Image Model """
+
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+
+class Comment(TimeStampedModel):
+
+    """ Comment Model """
+
+    message = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
+
+class Like(TimeStampedModel):
+
+    """ Like Model """
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
